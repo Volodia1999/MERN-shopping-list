b2f1c6c9380a836f0056a6b84cad82f2e26643b0
Volodymyr <revuravova@gmail.com>
2020-01-06 12:18:59 +0200

    Updates

---
+++ precache-manifest.8525d5e676f69383a52dc0d5e6121e2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4c18dc899a25a582e9002d0438ae51f",
+    "url": "/MERN-shopping-list/index.html"
+  },
+  {
+    "revision": "4a9066c41cc4fd68ab38",
+    "url": "/MERN-shopping-list/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "0ce8bea7008cd19f4c4f",
+    "url": "/MERN-shopping-list/static/css/main.c0e9d907.chunk.css"
+  },
+  {
+    "revision": "4a9066c41cc4fd68ab38",
+    "url": "/MERN-shopping-list/static/js/2.955cc50d.chunk.js"
+  },
+  {
+    "revision": "87984cad984dae3222b2a1d0861003a4",
+    "url": "/MERN-shopping-list/static/js/2.955cc50d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0ce8bea7008cd19f4c4f",
+    "url": "/MERN-shopping-list/static/js/main.eeed0a49.chunk.js"
+  },
+  {
+    "revision": "0247d4865b7d9bdc4a3e",
+    "url": "/MERN-shopping-list/static/js/runtime-main.434dbbbb.js"
+  }
+]);
